884adc7a08066f00f041785393be2101837900cc
shashikumar28 <sammysingh920518@gmail.com>
2025-04-24 17:04:41 +0530

    start js

---
+++ day-9/app.js
@@ -0,0 +1,26 @@
+let sam = 100;
+let sam1 = 500;
+
+
+
+//-------------
+// string
+
+let FirstName = `sam`;
+let LastName = 'kumar';
+let space = " ";
+
+console.log(FirstName);
+console.log(LastName);
+console.log(space)
+
+
+
+//---------------//
+
+// string concatination (concat) -> +
+console.log(FirstName + LastName);
+console.log(FirstName + space + LastName);
+console.log(FirstName + " " + LastName);
+console.log(FirstName + ' ' + LastName);
+console.log(FirstName + ` ` + LastName);
