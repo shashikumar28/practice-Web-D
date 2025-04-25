0e1aa6113bcfb69e26c8ce296d31d093ed65e299
shashikumar28 <sammysingh920518@gmail.com>
2025-04-25 17:28:05 +0530

    todays's code

---
+++ day-10/backticks/app.js
@@ -0,0 +1,35 @@
+let FirstName = "John"; 
+let LastName = "Doe";
+
+
+let Age = 30;
+let City = "New York";
+let Country = "USA";
+let Occupation = "Software Engineer";
+
+
+let FavName = "eeeeeeeehhhhhhhhhh";
+let FavColor = "Blue";
+
+
+geetings = "mere pehle name"  + FirstName + "or mere usse pehle name"  + LastName + "or meri aage abhi" + Age + "or me rehna wala" + City + "or mere permanent address" + Country + "or mere Occupation ha" + Occupation + "or mere favourite name" + FavName + "or mere favourite color" + FavColor;
+console.log(geetings);
+// console.log("My name is " + FirstName + " " + LastName + ". I am " + Age + " years old. I live in " + City + ", " + Country + ". I work as a " + Occupation + ". My favorite name is " + FavName + " and my favorite color is " + FavColor + ".");
+// console.log(`My name is ${FirstName} ${LastName}. I am ${Age} years old. I live in ${City}, ${Country}. I work as a ${Occupation}. My favorite name is ${FavName} and my favorite color is ${FavColor}.`);
+
+
+geetings2 = `mere pehle name  ${FirstName} or mere usse pehle name   ${LastName}  or meri aage abhi  ${Age}  or me rehna wala  ${City} or mere permanent address  ${Country}  or mere Occupation ha  ${Occupation} or mere favourite name ${FavName}  or mere favourite color  ${FavColor}`
+
+console.log(geetings2);
+
+
+
+
+console.log(FirstName);
+console.log(LastName );
+console.log(Age );
+console.log(City );
+console.log(Country);
+console.log(Occupation);
+console.log(FavName);
+console.log(FavColor);
---
+++ day-10/boolean/app.js
@@ -0,0 +1,26 @@
+// let hell  = true;
+// console.log(hell);
+
+
+
+// let other = false;
+// console.log(other);
+
+
+
+// // /            / 
+
+// let score = 100;
+// // let ans = score == 100;  using comparison operator true because score value is 100 this true
+
+// let ans = score == 440;   //comparison operator false because score value is 100 and i give 440 so then showing me false console and score ki value ki 100 ha ham compare kar rhe ha ki 100 100 == ke brabar ha ki ni 
+
+// console.log(ans)
+
+//  console.log(typeof(ans));
+
+
+
+
+
+ 
