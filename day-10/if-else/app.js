044e30b5e9af1e7aeb0168b94277686fff386f07
shashikumar28 <sammysingh920518@gmail.com>
2025-04-28 17:37:16 +0530

    if and else

---
+++ day-10/if-else/app.js
@@ -0,0 +1,42 @@
+if(true){console.log("mai swarg me hi")};
+
+console.log(true);
+
+if(true);{
+    console.log("me ghumne ja rha hu ");
+}
+
+if(true);{
+    console.log("me ghumne ja rha hu ");
+}
+
+let score = 32;
+// let score = 34;
+
+if(score >= 32){
+    console.log("pappu pass  ho gya");
+}
+else{
+    console.log("beta tumse na ho payega ")
+}
+
+
+let passing = 92;
+
+if(passing <=  90){
+    console.log("A+")
+}
+else if (passing <+ 80){
+    console.log("A")
+}
+
+else if (passing <= 70) {
+    console.log("B+")
+
+}
+else if (passing <= 60){
+    console.log("B")
+}
+else{
+    console.log("tujhse nahi hoga beta")
+}
