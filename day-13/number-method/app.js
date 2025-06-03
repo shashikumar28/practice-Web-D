522372524b41312fc912143148dee7ad44027122
shashikumar28 <sammysingh920518@gmail.com>
2025-06-03 15:08:50 +0530

    number-method-part-2

--- day-13/number-method/app.js
+++ day-13/number-method/app.js
@@ -1,9 +1,37 @@
-let num = 999;//
+// let num = 999;//
 // console.log(num.length);
-console.log(num.length());//kuki num.length array par kaam krta ha
+// console.log(num.length());//kuki num.length array par kaam krta ha
 
 
 
 
 // ---------------------------------------
+// console.log(Math.floor(Math.random() * 5) + 1);
+// console.log(Math)
 
+// console.log(Math.LN2)
+// console.log(Math.LN10)
+
+// console.log(Math.PI) 
+
+// console.log(Math.floor(Math.random() * 9000) + 1000);
+
+// console.log(Math.floor(Math.random() * 999) + 100)
+ 
+
+//  console.log( Math.floor( (Math.random() * 95) + 5 ) );
+
+// console.log(Math.ceil(4.3))
+// console.log(Math.floor(4.7))
+// console.log(Math.round(4.4))//4
+// console.log(Math.round(4.5))//5
+
+// console.log(Math.pow(2,5))
+// console.log(Math.pow(2,13))
+
+
+// console.log(Math.random())
+
+// console.log(Math.random()* 10)
+
+console.log( Math.floor(Math.random() * 1000) );
