97d5a7942fdc51538c353717821403be7b354c4d
shashikumar28 <sammysingh920518@gmail.com>
2025-06-05 18:10:56 +0530

    string-method

--- day-13/method/app.js
+++ day-13/method/app.js
@@ -1,43 +1,43 @@
+let mark = {
+    dsa : 10,
+    web : 25,
+    ml : 25,
+    totalMarks : function(){
+        return  mark.dsa + this.web + this.ml
+
+    }
+
+ }
+ console.log(mark.totalMarks())
+
+
+
+// //  let mark = {
+// //     dsa : 10,
+// //    web : 25,
+// //      ml : 25,
+// //      totalMarks : function(){
+// //          return  10 + 25 + 25
+
+// //      }
+// //     }
+
+// //   console.log(mark.totalMarks())
+
+
+
+
 // let mark = {
-//     dsa : 10,
-//     web : 25,
-//     ml : 25,
-//     totalMarks : function(){
-//         return  this.dsa + this.web + this.ml
-
-//     }
-
-// }
-// console.log(mark.totalMarks())
-
-
-
-//  let mark = {
 //     dsa : 10,
 //    web : 25,
 //      ml : 25,
-//      totalMarks : function(){
-//          return  10 + 25 + 25
+//      totalMarks : function(sam){
+//          return  10 + 25 + 25 - sam
 
 //      }
 //     }
 
-//   console.log(mark.totalMarks())
-
-
-
-
-let mark = {
-    dsa : 10,
-   web : 25,
-     ml : 25,
-     totalMarks : function(sam){
-         return  10 + 25 + 25 - sam
-
-     }
-    }
-
-  console.log(mark.totalMarks(50))
+//   console.log(mark.totalMarks(50))
 
 
 
--- day-13/string-method/app.js
+++ day-13/string-method/app.js
@@ -1,4 +1,27 @@
-// let str = "shashi"
+ let str = "Shashi   "
 
 
 // console.log(str.length)
+ //let ans = str.toUpperCase().trim().length
+ //console.log(ans)
+
+
+
+//  let ans  = str.trim().
+
+//  let ans  = str.trim().length
+
+
+// let ans = str.toLowerCase()
+
+// let ans = str.includes("H")//false capital h nahi ha h me output true
+// let ans = str.includes("s")//small s ha string me isliye output true
+
+// let ans = str.includes("sha ") false kyuki sha ke bad shas ha isliye false
+
+// let ans = str.includes("SHASHI")//string me first letter capital ha and uske bad sab small isliye false
+
+
+
+
+ console.log(ans)
