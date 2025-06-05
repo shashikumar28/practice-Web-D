746bfd37ee594074850f60989bf7233cee8b6814
shashikumar28 <sammysingh920518@gmail.com>
2025-06-05 15:24:07 +0530

    method-number

---
+++ day-13/method/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+ console.log(obj[key]) //like arrays
+    console.log(obj[subject]); //value
+    console.log(obj.subject); //undefined
+    console.log(obj.first); //undefined
+    console.log(obj.math); //30
---
+++ day-13/string-method/app.js
@@ -0,0 +1,4 @@
+// let str = "shashi"
+
+
+// console.log(str.length)
