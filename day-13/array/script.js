d53f7cbcf39aee6ce7e0f1489cdd513928ff81ad
shashikumar28 <sammysingh920518@gmail.com>
2025-06-06 14:26:13 +0530

    array-and-arrmethod

---
+++ day-13/array/script.js
@@ -0,0 +1,39 @@
+// console.log(Math.floor(Math.random() * 17 )  + 17)
+
+// console.log(Math.floor(Math.random() * 20 )  + 0)
+
+// console.log(Math.floor(Math.random() * 21 )  + 13)
+
+// console.log(Math.floor(Math.random() * 90 )  + 9)
+
+// let sam = [10,20,"sam" , 201, 66 , 25, true , Boolean];
+
+// console.log(sam)
+
+// console.log(sam[1])
+// console.log(sam[5])
+// console.log(sam[3])
+// console.log(sam[2])
+// console.log(sam[6])
+ 
+// console.log(sam[7]) 
+
+
+
+
+let sam = [10,20,"sam" , 201,[120,130,[100,200,300],140], 66 , 25, true , ];
+
+// console.log(sam[4][3]) 
+// sam = [4][3]  = "shashi"
+// console.log(sam)
+
+
+// -----------------------------------------
+
+// sam.push(30)//ya last me value add karta ha 
+
+// console.log(sam)
+
+
+// sam.pop(20) //ya last se dlt krta ha 👍
+console.log(sam)
