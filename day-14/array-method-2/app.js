dbecc3859bd2d954a4d38ba7e3b456f23daf90d3
shashikumar28 <sammysingh920518@gmail.com>
2025-06-09 17:34:46 +0530

    loops and array

---
+++ day-14/array-method-2/app.js
@@ -0,0 +1,46 @@
+
+// // for(i = 1; i<=10; i++){
+// //     console.log(i)
+// // }
+
+
+// let i = 0;
+// // while(i < 10){
+//     i++;
+//     console.log(i)
+// }
+
+
+// let arr = [10,20,30,40,50]
+
+// console.log(arr)
+// arr.unshift(60)// ya starting me add karta ha 
+// console.log(arr)
+
+// arr.shift()
+// console.log(arr)//ya starting se remove karta ha 
+
+// arr.join(-->);
+// console.log(arr.join("--->"))//join ko hamesha string me dena hota ha 
+
+// console.log(arr.join("shashi"))
+// console.log(arr.join("❌❤️"))
+
+let arr = [10,20,30,40,50]
+
+
+// console.log(arr)
+
+// arr.includes(10)//aise nahi likhte includes ko console ke andar likte ha
+// console.log(arr.includes(10))
+
+// console.log(arr.includes("sam"))
+// console.log(arr.includes(40))
+// console.log(arr.includes(true))
+
+
+
+
+
+
+
