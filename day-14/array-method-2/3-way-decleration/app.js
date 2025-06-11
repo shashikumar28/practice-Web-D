196668e3004378bf8bbd726a61abd57033fb0c59
shashikumar28 <sammysingh920518@gmail.com>
2025-06-11 18:14:39 +0530

    let-const-var

---
+++ day-14/array-method-2/3-way-decleration/app.js
@@ -0,0 +1,77 @@
+// let ----var-------const
+
+//  -------------LET-------------------
+
+// let someone = 100;
+// console.log(someone);
+//  someone = "shashi";// right path i am trying to relinisilce
+//  console.log(someone)
+
+
+//  let someone = "kuchi bhi "//error i ma trying to redclear
+
+//  ---------------------------let ke case me redecalear nahi hota❌ reinilice hota ha 👍
+
+
+// -----------------------------------------------VAR-------------------------------------
+
+
+// var something = 100;
+// console.log(something)
+// var something = "tu mere bhai "
+// console.log(something)
+
+// something = 'true'
+// console.log(something)
+
+
+// ----------------------------------let ke case me redecalear  hota  reinilice  bhi hota ha hota ha 👍
+
+
+
+
+
+
+// --------------------------------const-----------------------------------------
+
+
+
+// const strenght = 100;
+// console.log(strenght)
+// // const strenght = "someone"//error const ke case me redclear nahi hota 
+
+// strenght = "kuch ni kr sakte"
+// console.log(strenght)//syntex error nahi ayega but runtime error ayega 
+
+
+// // let ke case me redecalear nahi hota❌ reinilice bhia nahi  hota ha ❌
+
+
+
+// // question   1
+
+
+
+// const shashi;
+// shashi = 100;//relinilise kr rhe ha but undefined ayega kyuki main variable me value nahi ha
+// console.log(shashi)
+
+
+// question  2
+
+// const person =  {
+//     name : 'shashi',
+//     language : 'hindi',
+//     age      : 22
+// }
+// console.log(person)
+// console.log(person.age)
+// console.log(person.name)
+// console.log(person.language)
+
+// person.name = "sammy"
+// person.language = "punjabi"
+//  console.log(person.name)
+//  console.log(person.language)
+
+ // ya isliye ho pa rha kyuki const only for person ko target kr rha ha na ki uske andar ki value ko 
