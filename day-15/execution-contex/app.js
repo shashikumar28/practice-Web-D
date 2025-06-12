8ce5a1732b8385d57432b3ab1c2e92c396da55b4
shashikumar28 <sammysingh920518@gmail.com>
2025-06-12 18:33:16 +0530

    gec-adv

--- day-15/execution-contex/app.js
+++ day-15/execution-contex/app.js
@@ -1,4 +1,3 @@
-
 // var username = "samarth";
 
 // var person = {
@@ -8,3 +7,32 @@
 
 
 // console.log(username);
+
+// -----------------------------
+
+
+// var username = "devi prasad";
+
+// function sum(){
+//     var num1 = 10;
+//     var num2 = 20;
+
+//     return num1+num2;
+// }
+
+// sum();
+// console.log(username)
+
+
+// -------------------------------
+
+var naam = "samarth"
+
+function sam(){
+    console.log("inside sam");
+    var a = 10;
+    console.log(a);
+}
+
+sam();
+console.log(naam);
