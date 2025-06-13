cbadd73ec4a9608a6734ca55af4ac478a1b59979
shashikumar28 <sammysingh920518@gmail.com>
2025-06-13 17:41:47 +0530

    gec-question

---
+++ day-15/question/app.js
@@ -0,0 +1,45 @@
+// var a = 10;
+
+
+// function mereFunction (){
+//     console.log("mere insidefunction")
+
+// }
+// console.log(a)
+
+
+// mereFunction()
+
+
+// -------------------------
+
+// ----- HOISTING -------
+// whenever we access the variable or the funciton before its declaration then it is called hoisting
+
+
+// console.log(a)
+
+
+// mereFunction()
+
+
+// var a = 10;
+
+
+// function mereFunction (){
+//     console.log("mere insidefunction")
+
+// }
+
+
+
+// -----------------------------------
+
+// also is it hoisting
+
+function a (){
+    console.log(b)
+    var b = 200
+}
+
+a();
