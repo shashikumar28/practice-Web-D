0e403404e8e008a762a89ddb754c44c2ac2a6b38
shashikumar28 <sammysingh920518@gmail.com>
2025-06-16 18:24:55 +0530

    hositing

---
+++ DAY-16/hositing/app.js
@@ -0,0 +1,82 @@
+// a();
+
+// function a (){
+//     console.log(a)
+//     var a = 10;
+// }//hositing ho rhi ha 
+
+  
+// b();
+
+// function b (){
+//     var a =  10;
+//     console.log(a)
+// }
+
+
+
+// -------------- let ke case me-------------------------
+
+
+// b();
+
+// function b (){
+//     console.log(a)
+//     let a =  10;//let ke case ReferenceError: Cannot access 'a' before initialization
+    
+// }
+
+// sam();
+
+// function sam (){
+//     let a = 10;
+//     console.log(a)
+// }//error ni ayega but hositing ho rhi ha 
+
+
+
+// -------------------------same let me jaise hota ha const me bhi waise hi hota ha-------------------
+
+
+
+
+// sam();
+
+// function sam (){
+//     const a = 10;
+//     console.log(a)
+// }
+
+
+
+
+
+// sam();
+
+// function sam (){
+//     console.log(a)//error ReferenceError: Cannot access 'a' before initialization
+//     const a = 10;
+    
+// }
+
+
+
+
+// question
+
+
+console.log(a)//un
+var a = 100;
+let w = 20;
+console.log(w)//20
+console.log(s)
+let s = 20;
+
+
+
+function b (){
+    console.log(n)//ReferenceError: Cannot access 'a' before initialization
+    let  n = 10
+}
+
+b();
