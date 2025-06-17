5505fde300f5452d2b52c1e6fa55804b7ab97cde
shashikumar28 <sammysingh920518@gmail.com>
2025-06-17 16:58:30 +0530

    scope-and-ques

---
+++ DAY-16/scope/app.js
@@ -0,0 +1,48 @@
+// function a (){
+//     var a = 10;
+//     console.log(a)
+// }
+// a();
+
+// // ---------------------
+
+
+// function outer (){
+//     var a = 10;
+// inner()
+
+
+//     function inner (){
+//         var  c = 20;
+//         console.log(c)
+
+//         }  
+//         console.log(a) 
+//      }
+//      outer()
+
+
+
+// ------------------------------------
+
+
+
+
+
+// ---------------------
+
+
+function outer (){
+    var a = 10;
+// inner()
+
+
+    //  function inner (){
+    {
+        // var  a = 20;
+        console.log(a)
+
+    }
+        console.log(a) 
+     }
+     outer();
