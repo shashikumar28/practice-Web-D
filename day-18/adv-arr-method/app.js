27ca04c243b32c509f771240f7bf4c1e6fdb656a
shashikumar28 <sammysingh920518@gmail.com>
2025-07-04 16:34:46 +0530

    adv-arr-method

---
+++ day-18/adv-arr-method/app.js
@@ -0,0 +1,58 @@
+//array
+
+
+//1.  forEach()
+
+// let arr = ['india' , 'uk', 'new zeeland', 'south africa'];
+
+//  forEach()  -> method which accept a cb function
+
+// arr.forEach();  -> syntex lihkne ka //HOF  -> high order funcion
+// arr.forEach(function(item){
+//     console.log(item)
+// });
+
+
+// arr.forEach(function(item , index){
+//     console.log(item + '-->'  + index )
+// });
+
+
+// 2. map()
+
+
+//  let arr = ['india' , 'uk', 'new zeeland', 'south africa'];
+//  let number = [10,20,30,40,50]
+
+
+// let newarr = arr.map(function(item){
+//     return item.toUpperCase();
+// })
+// console.log(arr)
+// console.log(newarr)
+
+
+//--------------
+
+// let newsqrArray = number.map(function(item , index){
+//     return item*index
+// })
+// console.log(number)
+// console.log(newsqrArray)
+
+
+//---------------
+// 3. filter -> method
+
+//  let number = [10,20,30,40,50]
+
+//  let filterArray = number.filter(function(item){
+//     if(item>=20 ){
+//         return true
+//  } else  {
+//     return false
+//  }
+//  })
+
+//  console.log(number)
+//  console.log(filterArray)
