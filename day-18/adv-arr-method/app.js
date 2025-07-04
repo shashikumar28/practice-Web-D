5312209319b1783afb4dabdf550b0cdc3843781f
shashikumar28 <sammysingh920518@gmail.com>
2025-07-04 17:04:33 +0530

    reduce()

--- day-18/adv-arr-method/app.js
+++ day-18/adv-arr-method/app.js
@@ -56,3 +56,21 @@
 
 //  console.log(number)
 //  console.log(filterArray)
+
+
+let number = [10,20,30,40,50]
+
+
+let reduceArray = number.reduce(function(accumulator, currentValue){ //accumulator → previous value ya total jo ab tak calculate hua hai.
+     return accumulator * currentValue                              //currentValue → abhi ka current element.//
+    // console.log(reduceArray)
+} , 1)// Multiply → use 1 as initial value ✅ sum  use any any calculation use 
+// console.log(number)
+console.log(reduceArray)
+//reduce kaise kam krta ha 
+// Kaise Kaam Karta Hai?
+// reduce() left se right chalta hai (pehle element se last tak)
+
+// Har step me accumulator ko update karta hai
+
+// Last me ek single final value return karta hai
