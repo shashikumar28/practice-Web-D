bcf75fc046bf1b1d44bf780610f0109a138a4c64
Shashi Kumar <sammysingh920518@gmail.com>
2025-09-02 00:51:22 +0530

    dom-1

---
+++ day-24/dom/app.js
@@ -0,0 +1,65 @@
+// document means html 
+
+//  let ans = document.getElementById('sam')
+// ans.style.color  = 'pink';
+// ans.style.fontSize = '40px';
+
+
+// ans.style.cssText =  `
+// font-size : 50px;
+// color : red;
+// background-color: green;
+// font-size: 50px ;
+// `
+
+
+let allheading = document.getElementsByTagName('h1');
+
+
+// for( let heading of  allheading){
+//     heading.style.color = 'red';
+//     heading.style.backgroundColor = 'black';
+// }
+
+
+// for( let heading of  allheading){
+//     heading.style.cssText = `
+//     color : red;
+//     background-color: green;
+//     font-size: 50px;
+    
+    
+//     `
+// }
+
+
+
+// let elem = document.getElementsByClassName('sammy');
+
+// for(let  lems of  elem){
+//     lems.style.textDecoration = "line-through";
+// }
+
+
+
+// class select
+// let qw1 = document.querySelector('.sammy');
+// let qw2 = document.querySelector('.sammy');
+
+// id select
+
+// let qx = document.querySelector('#sam')
+
+// let nar = document.querySelector('p')
+
+//id
+// let ans = document.querySelectorAll('#sam')
+//class
+// let ans = document.querySelectorAll('.sam')
+let ans = document.querySelectorAll('p')
+
+
+
+
+
+
