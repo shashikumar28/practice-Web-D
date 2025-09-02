b5b06d221eb61e905e452115982b40805ca19c2d
Shashi Kumar <sammysingh920518@gmail.com>
2025-09-02 14:20:56 +0530

    dom-properties

---
+++ day-24/dom-properties/app.js
@@ -0,0 +1,18 @@
+let p = document.querySelector('p');
+
+
+// console.log(p.innerHTML); inner html sabkuch de dega andar se
+
+// console.log(p.innerText)//ya sirf text dega
+
+// console.log(p.textContent)
+
+//p.innerHTML = "hi am professinal coder"
+
+// p.textContent = 'hi you are not eligible'
+
+// p.innerText = "currently i ma leaner"
+   
+
+// console.log(a);
+// console.log(a);
