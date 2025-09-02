1e155d4bc801e24af6b676d5afc2f6d9186cf7e3
Shashi Kumar <sammysingh920518@gmail.com>
2025-09-02 18:39:20 +0530

    dom-tree

---
+++ day-25/dom-tree/app.js
@@ -0,0 +1,30 @@
+let div = document.querySelector('div')
+
+//. 1. children
+
+// console.log(div)
+// console.log(div.children)
+// console.log(div.children[0])
+// console.log(div.children[1])
+
+
+//et art = document.querySelector('article')
+// console.log(art.children)
+// console.log(art.children[0].children[0].children[0])
+
+ //. 2  parent
+
+//  console.log(div.parentElement)
+//   console.log(div.parentElement.parentElement)
+//   console.log(div.parentElement.parentElement.parentElement)
+//   console.log(div.parentElement.parentElement.parentElement.parentElement)
+//   console.log(div.parentElement.parentElement.parentElement.parentElement.parentElement)
+
+
+//----------------
+
+let art = document.querySelector('h1')
+
+// console.log(art.nextElementSibling)
+// console.log(art.nextElementSibling.nextElementSibling)
+// console.log(art.nextElementSibling.previousElementSibling)
