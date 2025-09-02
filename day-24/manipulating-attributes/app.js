265b88946d5d49997f3254ed679f5e4215df6680
Shashi Kumar <sammysingh920518@gmail.com>
2025-09-02 15:55:25 +0530

    setinterval-clearinterval

--- day-24/manipulating-attributes/app.js
+++ day-24/manipulating-attributes/app.js
@@ -1,12 +1,12 @@
-let num = document.querySelector('a');
+// let num = document.querySelector('a');
 
-//console.log(num.getAttribute('href'))
+// //console.log(num.getAttribute('href'))
 
-let inp = document.querySelector('input')
-//onsole.log(inp.getAttribute('type'))
-// //
-num.setAttribute('href' , 'http://facebook.com');
+// let inp = document.querySelector('input')
+// //onsole.log(inp.getAttribute('type'))
+// // //
+//// num.setAttribute('href' , 'http://facebook.com');
 
-inp.setAttribute('type' , 'color')
-inp.setAttribute('type' , 'date')
-inp.setAttribute('type' , 'checkbox')
+// inp.setAttribute('type' , 'color')
+// inp.setAttribute('type' , 'date')
+// inp.setAttribute('type' , 'checkbox')
---
+++ day-24/setinterval/app.js
@@ -0,0 +1,8 @@
+ let idd = setInterval(function(){
+console.log('hi am swaggy')
+} , 2000)
+
+
+setTimeout(function() {
+clearInterval(idd)
+ } , 10000)
