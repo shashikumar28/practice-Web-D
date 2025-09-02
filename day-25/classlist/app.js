6ec9238c1988fe542263c3cc198f2409ae1f2ef6
Shashi Kumar <sammysingh920518@gmail.com>
2025-09-02 17:53:45 +0530

    classlist

---
+++ day-25/classlist/app.js
@@ -0,0 +1,26 @@
+let h1 = document.querySelector('h1');
+let h2 = document.querySelector('h2');
+let h3 = document.querySelector('h3')
+
+//. 1 add
+// h1.classList.add('class1')
+// h2.classList.add('class2')
+// h3.classList.add('class3')
+
+
+h1.classList.add('class1' , 'class2' , 'class3')
+
+//.  2  remove
+
+//h1.classList.remove('class1')
+// h2.classList.remove('class2')
+// h3.classList.remove('class3')
+
+//h1.classList.remove('class1' , 'class2' , 'class3')
+
+
+//.  3 toggle. 
+
+//h1.classList.toggle('class1' , 'class2'❌)//ek bar me ek hi not allowed multiple
+
+
