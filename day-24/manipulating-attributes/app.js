0e37f6a5766abbd26ebdd707feedc8b7240bd035
Shashi Kumar <sammysingh920518@gmail.com>
2025-09-02 15:04:30 +0530

    setattributes

---
+++ day-24/manipulating-attributes/app.js
@@ -0,0 +1,12 @@
+let num = document.querySelector('a');
+
+//console.log(num.getAttribute('href'))
+
+let inp = document.querySelector('input')
+//onsole.log(inp.getAttribute('type'))
+// //
+num.setAttribute('href' , 'http://facebook.com');
+
+inp.setAttribute('type' , 'color')
+inp.setAttribute('type' , 'date')
+inp.setAttribute('type' , 'checkbox')
