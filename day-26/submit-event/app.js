44ac41cabcaa17a8413f933c2a5b88789c572e9d
Shashi Kumar <sammysingh920518@gmail.com>
2025-09-03 17:56:21 +0530

    submit-event

---
+++ day-26/submit-event/app.js
@@ -0,0 +1,23 @@
+// let form = document.querySelector('form')
+
+// form.addEventListener('submit' , ()=>{
+//     console.log("form submited")
+// })
+
+
+
+
+let form = document.querySelector('form')
+
+form.addEventListener('submit' , (event)=>{
+    event.preventDefault();
+    console.log(form.elements)
+        console.log(form.elements)[0]
+            console.log(form.elements[1])
+                        console.log(form.elements[0].value)
+                                                console.log(form.elements[1].value)
+
+
+
+
+})
